340f50eafb1e02d27037e5d56d357693835876d7
Aleksandr <mail.bolshakov@gmail.com>
2021-02-10 14:51:53 +0300

    Initial commit

---
+++ src/main/java/pro/bolshakov/research/ibmmq/IbmMqExampleApplication.java
@@ -0,0 +1,13 @@
+package pro.bolshakov.research.ibmmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbmMqExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IbmMqExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/bolshakov/research/ibmmq/IbmMqExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.bolshakov.research.ibmmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IbmMqExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

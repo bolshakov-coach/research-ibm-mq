2c452a24e8c23e5ab3fd0564820320a9d535ad1c
Aleksandr <mail.bolshakov@gmail.com>
2021-02-11 10:20:37 +0300

    modified example with options of launching

--- src/main/java/pro/bolshakov/research/ibmmq/service/JmsListenerService.java
+++ src/main/java/pro/bolshakov/research/ibmmq/service/JmsListenerService.java
@@ -1,10 +1,16 @@
 package pro.bolshakov.research.ibmmq.service;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
 
 @Component
+@ConditionalOnProperty(value = "listen.jms", havingValue = "true")
 public class JmsListenerService {
+
+    public JmsListenerService() {
+        System.out.println("JmsListenerService created");
+    }
 
     @JmsListener(destination = "${ibm.mq.queue2}")
     public void receiveMessage(String message){
